1f4b959ece97b4b80f9e3640f983eefb1233e992
ferdelamad <fernando.delamadrid@checkr.com>
2019-04-22 22:27:45 -0700

    Connect frontend with apollo-client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+import Launches from './components/Launches';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:6001/graphql'
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Space X App</h1>
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
